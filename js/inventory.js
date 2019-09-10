d132887cc3dd03c723ead8c9522c236e13df41c8
mikeosborne90 <mikeosborne1990@gmail.com>
2019-09-09 20:51:25 -0700

    Added form, need to change values to match groceryItems.

--- js/inventory.js
+++ js/inventory.js
@@ -127,4 +127,85 @@ const addInventoryToDOM = function (list) {
 //Remember that array full of the produce I, you created? Well, now it serves here to loop through and append to the container. Done.
 addInventoryToDOM(inventoryList);
 
+function createLabel(forValue, textValue)
+{
+    const label = document.createElement("label");
 
+    label.setAttribute("for", forValue);
+    label.innerText = textValue;
+
+    return label;
+}
+
+function createInput(typeValue, nameValue, placeholderValue, classValue, idValue)
+{
+    const input = document.createElement("input");
+
+    input.type = typeValue;
+    input.name = nameValue;
+    input.placeholder = placeholderValue;
+    input.className = classValue;
+    input.id = idValue;
+
+    return input;
+}
+
+//receives array of labels and array of inputs
+function createForm(name, method, action, labels, inputs)
+{
+    const form = document.createElement("form");
+
+    form.name = name;
+    form.method = method;
+    form.action = action;
+
+    if(labels.length == inputs.length) // equal # of labels and inputs
+    {
+        for(i = 0; i < labels.length; i++)
+        {
+            form.appendChild(labels[i]);
+            form.appendChild(inputs[i]);
+        }
+    }
+    else
+    {
+        console.log("Form # of labels and inputs do not match!");
+    }
+
+    const button = document.createElement("button");
+    button.className = "btn-primary btn";
+    button.type = "submit";
+    button.innerText = "Submit";
+
+    form.appendChild(button);
+
+    return form;
+}
+
+//----------------Add labels and inputs to Arrays-------------------
+
+let myLabels = [];
+let label1 = createLabel("email", "Email");
+let label2 = createLabel("subject", "Subject");
+let label3 = createLabel("message", "Message");
+
+myLabels.push(label1);
+myLabels.push(label2);
+myLabels.push(label3);
+
+let myInputs = [];
+let input1 = createInput("email", "email", "Enter Email Here","form-control", "email");
+let input2 = createInput("text", "subject", "Enter Subject Here","form-control", "subject");
+let input3 = createInput("text", "message", "Enter Message Here","form-control", "message");
+
+myInputs.push(input1);
+myInputs.push(input2);
+myInputs.push(input3);
+
+//----------Call createForm Function and attach to div.content-------------
+
+let myForm = createForm("basicForm", "GET", "js/form-submission.js", myLabels, myInputs);
+
+const inventoryForm = document.getElementById("inventoryForm");
+
+inventoryForm.appendChild(myForm);
